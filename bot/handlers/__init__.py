16a6a44448338ffa0ab22d5e2a8c5e341cb1b25e
MOHAMMED AL-HADDAD <eng.massa.ye@gmail.com>
2025-08-31 19:45:20 +0300

    feat: add duplicate message deletion option

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,7 +3,11 @@
 from .start import start
 from .navigation import echo_handler
 from .topics import insert_sub_conv, insert_sub_private
-from .ingestion import ingestion_handler, duplicate_callback
+from .ingestion import (
+    ingestion_handler,
+    duplicate_callback,
+    duplicate_cancel_callback,
+)
 from .groups import insert_group_conv, insert_group_private
 from .admins import admins_conv
 from .approvals import approvals_handler, approval_callback
@@ -17,6 +21,7 @@ __all__ = [
     "insert_sub_private",
     "ingestion_handler",
     "duplicate_callback",
+    "duplicate_cancel_callback",
     "insert_group_conv",
     "insert_group_private",
     "admins_conv",
